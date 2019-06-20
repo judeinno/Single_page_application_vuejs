2c11d876c325f00adca3c9e0ce6f945e1591d7a5
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-06-20 11:49:41 +0300

    My first vue js component

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 import Vue from 'vue'
 
+Vue.component('app', {
+  // templete with div having ID app for intial binding
+  template: `
+    <div id="app">
+      <nav class="nav has-shadow">
+        <div class="container">
+          <a href="/">
+            <img src="http://bit.ly/vue-img"
+              alt="Vue Intro" />
+          </a>
+        </div>
+      </nav>
+
+      <section class="main-section section"></section>
+      <footer class="footer">
+        <div class="container">
+          <div class="content has-text-centered">
+            Follow us on
+            <a href="https://twitter.com/judeinno" target="_blank">Twitter</a>
+          </div>
+        </div>
+      </footer>
+    </div>`
+})
+
 const app = new Vue({
-  data: {
-    hello: 'Hi there25'
-  },
-  // Helps vueJs know how to rerender the section that its mounted on
-  template: '<div id="app">{{hello}}</div>'
+  // Vue JS property that returns an element
+  // Same as templete: '<app></app>'
+  render: h => h('app')
 })
 
 export { app }
