c0f9f2c36a15ebbf5bedc2ff4aaf5c6263bf86ef
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-06-20 12:03:42 +0300

    Add vue loader - remove hot reloading function confige that load the templets on the fly since loader is doing it.

--- src/app.js
+++ src/app.js
@@ -1,34 +1,16 @@
 import Vue from 'vue'
+import AppLayout from '../theme/Layout.vue'
 
 Vue.component('app', {
   // templete with div having ID app for intial binding
   template: `
-    <div id="app">
-      <nav class="nav has-shadow">
-        <div class="container">
-          <a href="/">
-            <img src="http://bit.ly/vue-img"
-              alt="Vue Intro" />
-          </a>
-        </div>
-      </nav>
-
-      <section class="main-section section"></section>
-      <footer class="footer">
-        <div class="container">
-          <div class="content has-text-centered">
-            Follow us on
-            <a href="https://twitter.com/judeinno" target="_blank">Twitter</a>
-          </div>
-        </div>
-      </footer>
-    </div>`
+    `
 })
 
 const app = new Vue({
-  // Vue JS property that returns an element
-  // Same as templete: '<app></app>'
-  render: h => h('app')
+
+  // render the templete
+  render: h => h(AppLayout)
 })
 
 export { app }
---
+++ theme/Layout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <nav class="nav has-shadow">
+        <div class="container">
+          <a href="/">
+            <img src="http://bit.ly/vue-img"
+              alt="Vue Intro" />
+          </a>
+        </div>
+      </nav>
+
+      <section class="main-section section"></section>
+      <footer class="footer">
+        <div class="container">
+          <div class="content has-text-centered">
+            Follow us on 
+            <a href="https://twitter.com/judeinno" target="_blank">Twitter</a>
+          </div>
+        </div>
+      </footer>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+
