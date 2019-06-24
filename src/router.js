833a843f30919ab41a63431159d7a81e2612c314
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-06-24 11:47:52 +0300

    Add extra route and enable history mode

--- src/router.js
+++ src/router.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Category from './theme/Category.vue'
+import Login from './theme/Login.vue'
 
 Vue.use(VueRouter)
 
-const routes = [{
-  path: '/',
-  component: Category
-}]
+const routes = [
+  { path: '/', component: Category },
+  { path: '/login', component: Login }
+]
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
