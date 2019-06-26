9f1c41f86f1cfb4326214146fcf45311b606cf2d
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-06-26 12:24:07 +0300

    Show how to use modules to change state and update the UI with the modules

--- src/theme/Category.vue
+++ src/theme/Category.vue
@@ -12,34 +12,28 @@
 
 <script>
   import Post from './Post.vue'
-  import appService from '../app.service.js'
+  import { mapGetters } from 'vuex'
 
   export default {
     components: {
       'app-post': Post
     },
 
-    data () {
-      return {
-        id: this.$route.params.id,
-        posts: []
-      }
+    computed: {
+      ...mapGetters('postsModule', ['posts'])
     },
     watch: {
       '$route' (to, from) {
-        this.id = to.params.id
         this.loadPosts()
       }
     },
     methods: {
       loadPosts () {
         let categoryId = 2
-        if (this.id === 'mobile') {
+        if (this.$route.params.id === 'mobile') {
           categoryId = 11
         }
-        appService.getPosts(categoryId).then(data => {
-          this.posts = data
-        })
+        this.$store.dispatch('postsModule/updateCategory', categoryId)
       }
 
     },
