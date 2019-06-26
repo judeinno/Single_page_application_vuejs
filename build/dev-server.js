031a7388c30ae52bee89e532fe7cd1c1997a918f
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-06-26 13:57:05 +0300

    Add server bundle call back that is called when the server bundle is updated

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,7 +1,9 @@
 const webpack = require('webpack')
 const clientConfig = require('./webpack.client.config')
+const serverConfig = require('./webpack.server.config')
+const MFS = require('memory-fs')
 
-module.exports = function setupDevServer (app) {
+module.exports = function setupDevServer (app, onUpdate) {
   // Extend config file and add a new entry point
   clientConfig.entry.app = [
     'webpack-hot-middleware/client',
@@ -21,4 +23,12 @@ module.exports = function setupDevServer (app) {
     })
   )
   app.use(require('webpack-hot-middleware')(clientCompiler))
+
+  const serverCompiler = webpack(serverConfig)
+  const mfs = new MFS()
+  const outputPath = path.join(serverConfig.output.path, 'server/main.js')
+  serverCompiler.outputFileSystem = mfs
+  serverCompiler.watch({}, () => {
+    onUpdate(mfs.readFileSync(outputPath, 'utf-8'))
+  })
 }
