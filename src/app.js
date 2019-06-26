dd437e314b57e451a88ada6cbfea14b2904c0243
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-06-26 11:17:43 +0300

    add Vuex, and use it to initialise the store plus add it to our app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import AppLayout from './theme/Layout.vue'
+import store from './vuex/index.js'
 import router from './router'
 
 const app = new Vue({
   router,
   // No need to call the render since app layout already has a render function
-  ...AppLayout
+  ...AppLayout,
+  // Include the store
+  store
 })
 
-export { app, router }
+export { app, router, store }
